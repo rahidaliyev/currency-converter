22ed7e441565d6c2bb1ea62979dbcdb9fb753c50
rahidaliyev <rahidaliyev18@gmail.com>
2022-04-24 01:21:25 +0400

    added new js

---
+++ script.js
@@ -0,0 +1,33 @@
+// const url=`https://api.exchangerate.host/latest?base=${from}&symbols=${to}`;
+let fromRubl = document.querySelector("#btn1");
+let fromUsd = document.querySelector("#btn2");
+let fromEur = document.querySelector("#btn3");
+let fromGbp = document.querySelector("btn4");
+let toRubl = document.querySelector("#btn5");
+let toUsd = document.querySelector("#btn6");
+let toEur = document.querySelector("#btn7");
+let toGbp = document.querySelector("btn8");
+
+fromRubl.addEventListener("click",myFunc);
+fromUsd.addEventListener("click",myFunc);
+fromEur.addEventListener("click",myFunc);
+fromGbp.addEventListener("click",myFunc);
+toRubl.addEventListener("click",myFunc);
+toUsd.addEventListener("click",myFunc);
+toEur.addEventListener("click",myFunc);
+toGbp.addEventListener("click",myFunc);
+let btn1=document.querySelectorAll("#btn-div-1 input");
+let btn2=document.querySelectorAll("#btn-div-2 input");
+console.log(btn1.value)
+function myFunc(){
+   const inputValue=btn1[0];
+   const outputValue=btn2[1];
+   fetch(`https://api.exchangerate.host/latest?base=${inputValue}&symbols=${outputValue}`)
+   .then(res => res.json())
+   .then(res=>{
+    const new_rate = res.rates[outputValue];
+   })
+}
+
+
+myFunc()
